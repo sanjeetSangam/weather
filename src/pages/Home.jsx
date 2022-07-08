6dd2e69607eaa28a77a229168c702896172aaa42
sanjeetSangam <factsbe0@gmail.com>
2022-07-08 22:40:43 +0530

    got current coordinates

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,17 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { Days } from "../components/Days";
 import { Search } from "../components/Search";
+import { Coordinates } from "../logic/defaultLocation";
 
 export const Home = () => {
-  useEffect(() => {}, []);
+  const [coordinates, setCoordinates] = useState({});
+
+  useEffect(() => {
+    Coordinates(setCoordinates);
+  }, []);
+
+  console.log(coordinates);
 
   return (
     <Container>
